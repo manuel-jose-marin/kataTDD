be53a5b794f46ab8e598159a816a746c48df7705
manuel-jose-marin <mj.marin@uniandes.edu.co>
2025-08-28 13:03:58 -0500

    Caso 3: Verde

--- kataTDDpractica/tests/test_conjunto.py
+++ kataTDDpractica/tests/test_conjunto.py
@@ -1,9 +1,8 @@
-
 ##CASOS DE PRUEBA##
 ## cuando no se tienen elementos, promedio() debe dar como respuesta un elemento nulo o un error
 ## caudno se tiene un solo elemento, promedio() debe ser el mismo elemento existente en el conjunto
 ## cuando se tienen dos elementos, promedio() debe ser la suma de los elementos del conjunto dividido entre 2
-## cuando se tienen n elementos, promedio() es la suma de los elementos del conjunto dividido el numero de elementos 
+## cuando se tienen n elementos, promedio() es la suma de los elementos del conjunto dividido el numero de elementos
 
 
 import unittest
@@ -11,12 +10,15 @@ from kataTDDpractica.src.katatddpractica.conjunto import Conjunto
 
 
 class TestConjunto(unittest.TestCase):
-    
+
     def test_conjunto_vacio(self):
         conjunto = Conjunto([])
         self.assertIsNone(conjunto.promedio())
-    
+
     def test_conjunto_un_elemento(self):
         conjunto = Conjunto([5])
         self.assertEqual(conjunto.promedio(), 5)
-    
+
+    def test_conjunto_dos_elementos(self):
+        conjunto = Conjunto([5, 7])
+        self.assertEqual(conjunto.promedio(), 6)
